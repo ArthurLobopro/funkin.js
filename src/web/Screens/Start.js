b49a598ed09a44f10b4094c6e9fc382bf3c4ada4
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-22 01:40:57 -0300

    update logo and add girlFriend

--- src/web/Screens/Start.js
+++ src/web/Screens/Start.js
@@ -1,79 +1,81 @@
 const { ipcRenderer } = require('electron')
 import { loadImage } from '../Util.js'
 import * as logoAnimation from "../../../assets/animations/logoBumpin.js"
+import * as girlFriendAnimation from "../../../assets/animations/gfDanceTitle.js"
 
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
-const appPath = ipcRenderer.sendSync('request-app-path')
-
-// const girlFriendAnimationPath = path.join(appPath, 'assets/animations/gfPixel.js')
-// const girlFriendAnimation = require()
-
-
 const images = {
-    // girlFriend: await loadImage(girlFriendAnimation.path),
+    girlFriend: await loadImage(girlFriendAnimation.path),
     logo: await loadImage(logoAnimation.path)
 }
 
 const Start = {
-    // girlFriend: {
-    //     ...girlFriendAnimation,
-    //     atualFrame: girlFriendAnimation.frames[0],
-    //     atualFrameIndex: 0,
-    //     attFrame() {
-    //         if (this.atualFrameIndex + 1 === this.frames.length) {
-    //             this.atualFrameIndex = 0
-    //             this.atualFrame = this.frames[0]
-    //         }
-
-    //         this.atualFrameIndex++
-    //         this.atualFrame = this.frames[this.atualFrameIndex]
-    //     },
-    //     draw() {
-    //         const { x: sx, y: sy, width, height } = this.atualFrame
-    //         ctx.drawImage(
-    //             images.girlFriend,
-    //             sx, sy,
-    //             width, height,
-    //             // canvas.width - width, canvas.height - height,
-    //             0,0,
-    //             width, height
-    //         )
-    //     }
-
-    // },
-    logo: {
-        ...logoAnimation,
-        atualFrame: logoAnimation.frames[0],
+    girlFriend: {
+        ...girlFriendAnimation,
+        atualFrame: girlFriendAnimation.frames[0],
         atualFrameIndex: 0,
         attFrame() {
-            if (this.atualFrameIndex + 1 === this.frames.length) {
-                this.atualFrameIndex = 0
-                this.atualFrame = this.frames[0]
-            }
-
-            this.atualFrameIndex++
+            this.atualFrameIndex = this.atualFrameIndex + 1 === this.frames.length ? 0 : this.atualFrameIndex + 1
             this.atualFrame = this.frames[this.atualFrameIndex]
         },
         draw() {
-            const { x: sx, y: sy, width, height } = this.atualFrame
+            const { x:sx, y:sy, width, height, frameHeight = 0, frameWidth = 0, frameX = 0 , frameY = 0 } = this.atualFrame
+            const x = canvas.width - width - 40 + frameX/2
+            const y = canvas.height - height - 20 + frameY
+            const dy = frameHeight || height
+            const dx = frameWidth || width
             ctx.drawImage(
-                images.logo,
-                sx, sy,
-                width, height,
-                // canvas.width - width, canvas.height - height,
-                -100,-75,
-                width / 1.5, height / 1.5
+                images.girlFriend, //Imagem
+                sx, sy, //Posição no sprite
+                width, height, //Largura e altura no sprite
+                x , y, //Posição x e y
+                dx, dy  //largura e altura na tela
+            )
+        }
+
+    },
+    logo: {
+        ...logoAnimation,
+        atualFrame: logoAnimation.frames[1],
+        atualFrameIndex: 0,
+        x: -130,
+        y: -90,
+        attFrame() {
+            this.atualFrameIndex = this.atualFrameIndex + 1 === this.frames.length ? 0 : this.atualFrameIndex + 1
+            this.atualFrame = this.frames[this.atualFrameIndex]
+        },
+        draw() {
+            const { width, height, frameHeight = 0, frameWidth = 0, frameX = 0 , frameY = 0 } = this.atualFrame
+            const sx = 0, sy = 0
+            const x = this.x + frameX
+            const y = this.y + frameY
+            const dy = frameHeight || height
+            const dx = frameWidth || width
+            ctx.drawImage(
+                images.logo, //Imagem
+                sx, sy, //Posição no sprite
+                width, height, //Largura e altura no sprite
+                x , y, //Posição x e y
+                dx, dy  //largura e altura na tela
             )
         }
     },
     
+    init(){
+        setInterval(() => Start.render(), 1000/30)
+        setInterval(() => {
+            Start.logo.attFrame()
+            Start.girlFriend.attFrame()
+        }, 1000/24)
+    },
+
     render() {
         ctx.fillStyle = "#000000"
         ctx.fillRect(0, 0, canvas.width, canvas.height)
         this.logo.draw()
-        // this.girlFriend.draw()
+        this.girlFriend.draw()
     },
 }
 
