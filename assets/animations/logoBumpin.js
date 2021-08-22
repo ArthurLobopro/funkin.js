fbfbb835211bd2ffb9de5a7f704e8e74a915ce8a
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-22 01:31:58 -0300

    animation files

---
+++ assets/animations/logoBumpin.js
@@ -0,0 +1,154 @@
+const { ipcRenderer } = require('electron')
+const path = require('path')
+
+const appPath = ipcRenderer.sendSync('request-app-path')
+const spritesFolder = path.resolve(appPath, "assets", "sprites")
+
+const spritePath = path.resolve(spritesFolder, 'logoBumpin.png')
+
+const animationFrames = [
+    {
+        x: 0,
+        y: 0,
+        width: 894,
+        height: 670,
+        frameX: -22,
+        frameY: -16,
+        frameWidth: 939,
+        frameHeight: 703
+    },
+    {
+        x: 904,
+        y: 0,
+        width: 939,
+        height: 703
+    },
+    {
+        x: 904,
+        y: 0,
+        width: 939,
+        height: 703
+    },
+    {
+        x: 0,
+        y: 713,
+        width: 911,
+        height: 683,
+        frameX: -14,
+        frameY: -10,
+        frameWidth: 939,
+        frameHeight: 703
+    },
+    {
+        x: 0,
+        y: 713,
+        width: 911,
+        height: 683,
+        frameX: -14,
+        frameY: -10,
+        frameWidth: 939,
+        frameHeight: 703
+    },
+    {
+        x: 921,
+        y: 713,
+        width: 905,
+        height: 679,
+        frameX: -17,
+        frameY: -12,
+        frameWidth: 939,
+        frameHeight: 703
+    },
+    {
+        x: 921,
+        y: 713,
+        width: 905,
+        height: 679,
+        frameX: -17,
+        frameY: -12,
+        frameWidth: 939,
+        frameHeight: 703
+    },
+    {
+        x: 921,
+        y: 713,
+        width: 905,
+        height: 679,
+        frameX: -17,
+        frameY: -12,
+        frameWidth: 939,
+        frameHeight: 703
+    },
+    {
+        x: 921,
+        y: 713,
+        width: 905,
+        height: 679,
+        frameX: -17,
+        frameY: -12,
+        frameWidth: 939,
+        frameHeight: 703
+    },
+    {
+        x: 921,
+        y: 713,
+        width: 905,
+        height: 679,
+        frameX: -17,
+        frameY: -12,
+        frameWidth: 939,
+        frameHeight: 703
+    },
+    {
+        x: 921,
+        y: 713,
+        width: 905,
+        height: 679,
+        frameX: -17,
+        frameY: -12,
+        frameWidth: 939,
+        frameHeight: 703
+    },
+    {
+        x: 921,
+        y: 713,
+        width: 905,
+        height: 679,
+        frameX: -17,
+        frameY: -12,
+        frameWidth: 939,
+        frameHeight: 703
+    },
+    {
+        x: 921,
+        y: 713,
+        width: 905,
+        height: 679,
+        frameX: -17,
+        frameY: -12,
+        frameWidth: 939,
+        frameHeight: 703
+    },
+    {
+        x: 921,
+        y: 713,
+        width: 905,
+        height: 679,
+        frameX: -17,
+        frameY: -12,
+        frameWidth: 939,
+        frameHeight: 703
+    },
+    {
+        x: 921,
+        y: 713,
+        width: 905,
+        height: 679,
+        frameX: -17,
+        frameY: -12,
+        frameWidth: 939,
+        frameHeight: 703
+    }
+]
+
+export { spritePath as path , animationFrames as frames  }
