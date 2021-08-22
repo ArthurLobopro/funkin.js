0de04481c4f5ae5c3832432248ad7c2dbe534eb0
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-22 13:08:17 -0300

    implement animationBase and added titleEnter

--- src/web/Screens/Start.js
+++ src/web/Screens/Start.js
@@ -1,28 +1,29 @@
-const { ipcRenderer } = require('electron')
-import { loadImage } from '../Util.js'
+const path = require('path')
+import { appPath } from '../../Paths.js'
+import { loadAudio, loadImage } from '../Util.js'
+import { animationBase } from '../Animation.js'
 import * as logoAnimation from "../../../assets/animations/logoBumpin.js"
 import * as girlFriendAnimation from "../../../assets/animations/gfDanceTitle.js"
+import * as titleEnterAnimation from "../../../assets/animations/titleEnter.js"
 
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
 const images = {
     girlFriend: await loadImage(girlFriendAnimation.path),
-    logo: await loadImage(logoAnimation.path)
+    logo: await loadImage(logoAnimation.path),
+    titleEnter: await loadImage(titleEnterAnimation.path)
 }
+
+const backgroundMusic = loadAudio(path.resolve(appPath, "assets/music/freakyMenu.ogg"))
+backgroundMusic.play()
 
 const Start = {
     girlFriend: {
-        ...girlFriendAnimation,
-        atualFrame: girlFriendAnimation.frames[0],
-        atualFrameIndex: 0,
-        attFrame() {
-            this.atualFrameIndex = this.atualFrameIndex + 1 === this.frames.length ? 0 : this.atualFrameIndex + 1
-            this.atualFrame = this.frames[this.atualFrameIndex]
-        },
+        ...new animationBase({ ...girlFriendAnimation }),
         draw() {
-            const { x:sx, y:sy, width, height, frameHeight = 0, frameWidth = 0, frameX = 0 , frameY = 0 } = this.atualFrame
-            const x = canvas.width - width - 40 + frameX/2
+            const { x: sx, y: sy, width, height, frameHeight = 0, frameWidth = 0, frameX = 0, frameY = 0 } = this.atualFrame
+            const x = canvas.width - width - 40 + frameX / 2
             const y = canvas.height - height - 20 + frameY
             const dy = frameHeight || height
             const dx = frameWidth || width
@@ -30,45 +31,58 @@ const Start = {
                 images.girlFriend, //Imagem
                 sx, sy, //Posição no sprite
                 width, height, //Largura e altura no sprite
-                x , y, //Posição x e y
+                x, y, //Posição x e y
                 dx, dy  //largura e altura na tela
             )
         }
 
     },
     logo: {
-        ...logoAnimation,
-        atualFrame: logoAnimation.frames[1],
-        atualFrameIndex: 0,
+        ...new animationBase({ ...logoAnimation }),
         x: -130,
         y: -90,
-        attFrame() {
-            this.atualFrameIndex = this.atualFrameIndex + 1 === this.frames.length ? 0 : this.atualFrameIndex + 1
-            this.atualFrame = this.frames[this.atualFrameIndex]
-        },
         draw() {
-            const { width, height, frameHeight = 0, frameWidth = 0, frameX = 0 , frameY = 0 } = this.atualFrame
+            const { width, height, frameHeight = 0, frameWidth = 0, frameX = 0, frameY = 0 } = this.atualFrame
             const sx = 0, sy = 0
             const x = this.x + frameX
             const y = this.y + frameY
-            const dy = frameHeight || height
-            const dx = frameWidth || width
+            const dh = frameHeight || height
+            const dw = frameWidth || width
             ctx.drawImage(
                 images.logo, //Imagem
                 sx, sy, //Posição no sprite
                 width, height, //Largura e altura no sprite
-                x , y, //Posição x e y
-                dx, dy  //largura e altura na tela
+                x, y, //Posição x e y
+                dw, dh  //largura e altura na tela
             )
         }
     },
-    
-    init(){
-        setInterval(() => Start.render(), 1000/30)
+    titleEnter: {
+        ... new animationBase({ ...titleEnterAnimation.frames.pressEnter }),
+        draw() {
+            const { width, height, x: sx, y: sy } = this.atualFrame
+            const dw = width 
+            const dh = height 
+            const x =  (canvas.width  - (dw / 2)) - canvas.width * 0.3
+            const y = canvas.height - dh - 50
+            console.table({x,y})
+            ctx.drawImage(
+                images.titleEnter, //Imagem
+                sx, sy, //Posição no sprite
+                width, height, //Largura e altura no sprite
+                x, y, //Posição x e y
+                dw, dh //largura e altura na tela
+            )
+        }
+    },
+
+    init() {
+        setInterval(() => Start.render(), 1000 / 30)
         setInterval(() => {
             Start.logo.attFrame()
             Start.girlFriend.attFrame()
-        }, 1000/24)
+            Start.titleEnter.attFrame()
+        }, 1000 / 24)
     },
 
     render() {
@@ -76,6 +90,7 @@ const Start = {
         ctx.fillRect(0, 0, canvas.width, canvas.height)
         this.logo.draw()
         this.girlFriend.draw()
+        this.titleEnter.draw()
     },
 }
 
