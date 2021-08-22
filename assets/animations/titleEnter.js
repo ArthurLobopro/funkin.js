8568b011a7a2cd3845a1e450fc35c644c81a545a
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-22 13:05:25 -0300

    Create titleEnter.js

---
+++ assets/animations/titleEnter.js
@@ -0,0 +1,345 @@
+const path = require('path')
+import { spritesFolder } from '../../src/Paths.js'
+
+const spritePath = path.resolve(spritesFolder, 'titleEnter.png')
+
+const enterPressed = [
+    {
+        x: 0,
+        y: 0,
+        width: 1477,
+        height: 79
+    },
+    {
+        x: 1487,
+        y: 0,
+        width: 1477,
+        height: 79
+    },
+    {
+        x: 0,
+        y: 0,
+        width: 1477,
+        height: 79
+    },
+    {
+        x: 1487,
+        y: 0,
+        width: 1477,
+        height: 79
+    },
+    {
+        x: 0,
+        y: 0,
+        width: 1477,
+        height: 79
+    },
+    {
+        x: 1487,
+        y: 0,
+        width: 1477,
+        height: 79
+    },
+    {
+        x: 1487,
+        y: 0,
+        width: 1477,
+        height: 79
+    },
+    {
+        x: 0,
+        y: 0,
+        width: 1477,
+        height: 79
+    },
+    {
+        x: 1487,
+        y: 0,
+        width: 1477,
+        height: 79
+    }
+]
+
+const pressEnter = [
+    {
+        x: 0,
+        y: 89,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 89,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 195,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 195,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 301,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 301,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 407,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 407,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 513,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 513,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 619,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 619,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 725,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 725,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 831,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 831,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 937,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 937,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 1043,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 1043,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 1149,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 1149,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 1255,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 1255,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 1255,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 1149,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 1361,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 1361,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 1467,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 1467,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 1573,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 1573,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 1679,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 1679,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 1785,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 1785,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 1891,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 1891,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 1997,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 1997,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 2103,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 2103,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 2209,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 1505,
+        y: 2209,
+        width: 1495,
+        height: 96
+    },
+    {
+        x: 0,
+        y: 89,
+        width: 1495,
+        height: 96
+    }
+]
+
+const frames = {
+    pressEnter: {
+        frames: pressEnter
+    },
+    enterPressed: {
+        frames: enterPressed
+    }
+}
+
+export { spritePath as path, frames }
