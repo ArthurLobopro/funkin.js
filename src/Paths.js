c92c073064a1566ea5b3e424852c979cf751f05a
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-23 00:25:00 -0300

    add paths

--- src/Paths.js
+++ src/Paths.js
@@ -3,6 +3,9 @@ const path = require('path')
 
 const appPath = ipcRenderer.sendSync('request-app-path')
 const spritesFolder = path.resolve(appPath, "assets", "sprites")
+const musicsFolder = path.resolve(appPath, "assets/music")
+const soundsFolder = path.resolve(appPath, "assets/sounds")
+const imagesFolder = path.resolve(appPath, "assets/images")
 
 
-export { appPath, spritesFolder }
+export { appPath, spritesFolder, musicsFolder, soundsFolder, imagesFolder }
