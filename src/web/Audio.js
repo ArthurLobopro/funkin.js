0105406fe985bf563e15d2d0bceda7b9dbf4dc09
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-24 23:15:23 -0300

    add scrollMenu audio

--- src/web/Audio.js
+++ src/web/Audio.js
@@ -3,11 +3,12 @@ import { musicsFolder, soundsFolder } from '../Paths.js'
 import { loadAudio } from './Util.js'
 
 const Music = {
-    menuMusic: loadAudio(path.resolve(musicsFolder, "freakyMenu.ogg")),
+    menuMusic: loadAudio(musicsFolder, "freakyMenu.ogg"),
 }
 
 const Sounds = {
-    selectMenu: loadAudio(path.resolve(soundsFolder, "confirmMenu.ogg"))
+    selectMenu: loadAudio(soundsFolder, "confirmMenu.ogg"),
+    scrollMenu: loadAudio(soundsFolder, "scrollMenu.ogg")
 }
 
 export { Music, Sounds }
