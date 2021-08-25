09b087ff129271461cc11a4643af85d1e0089623
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-25 00:02:21 -0300

    update loadImage to accept path segments

--- src/web/Util.js
+++ src/web/Util.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
-const loadImage = async path => {
+const loadImage = async (...pathSegments) => {
     const image = new Image()
-    image.src = path
+    image.src = path.resolve(...pathSegments)
     return new Promise(res => {
         image.onload = () => {
             res(image)
