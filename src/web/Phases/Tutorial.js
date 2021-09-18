66ea79142c7cbe931e4f31a7c3e1f57c53d30e67
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-09-18 18:20:13 -0300

    Implement Pause for tests

--- src/web/Phases/Tutorial.js
+++ src/web/Phases/Tutorial.js
@@ -5,6 +5,7 @@ import { frames as GirlFriendFrames } from "../../../assets/animations/GF-assets
 import { types as BoyfriendTypes } from "../../../assets/animations/BOYFRIEND.js"
 import { Images, Sprites } from "../Images.js";
 import { multframesAnimations } from "../Animation.js";
+import { Pause } from "../Screens/Pause.js";
 
 const images = {
     stageback: Images.stageback,
@@ -17,19 +18,19 @@ const images = {
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
-const girlFriend = new multframesAnimations({
+const GirlFriend = new multframesAnimations({
     frames: GirlFriendFrames.dancingBeat,
     types: GirlFriendFrames,
     x: 0,
     y: 0,
     width: 0,
     height: 0,
-    draw(){
+    draw() {
         const { x: sx, y: sy, width, height, frameHeight = 0, frameWidth = 0, frameX = 0, frameY = 0 } = this.atualFrame
         const dh = (frameHeight || height) / 1.5
         const dw = (frameWidth || width) / 1.5
-        const x = canvas.width / 2 - dw/2
-        const y = canvas.height /2 - dh / 2
+        const x = canvas.width / 2 - dw / 2
+        const y = canvas.height / 2 - dh / 2
         this.y = y
         this.x = x
         this.width = dw
@@ -47,13 +48,13 @@ const girlFriend = new multframesAnimations({
 const Boyfriend = new multframesAnimations({
     frames: BoyfriendTypes.frames["idle-dance"],
     types: BoyfriendTypes,
-    draw(){
+    draw() {
         const { x: sx, y: sy, width, height, frameHeight = 0, frameWidth = 0, frameX = 0, frameY = 0 } = this.atualFrame
         const dh = (frameHeight || height) / 1.5
         const dw = (frameWidth || width) / 1.5
-        const x = girlFriend.x + girlFriend.width / 2
-        const y = girlFriend.y + girlFriend.height / 2
-        
+        const x = GirlFriend.x + GirlFriend.width / 2
+        const y = GirlFriend.y + GirlFriend.height / 2
+
         ctx.drawImage(
             images.Boyfriend, //Imagem
             sx, sy, //Posição no sprite
@@ -65,35 +66,74 @@ const Boyfriend = new multframesAnimations({
 })
 
 const Tutorial = {
+    updateInterval: null,
     renderInterval: null,
-    render(){
+    isPaused: false,
+    pauseScreen: null,
+
+    render() {
         const { width, height } = images.stagefront
         ctx.drawImage(
             images.stageback,
             0, 0,
             canvas.width, canvas.height
         )
-        
+
         ctx.drawImage(
             images.stagefront,
             0, 400,
             canvas.width, height
         )
-        
+
         ctx.drawImage(
             images.curtains,
             0, 0,
             canvas.width, canvas.height
         )
 
-        girlFriend.draw()
+        GirlFriend.draw()
         Boyfriend.draw()
+
     },
 
-    init(){
-        this.renderInterval = setInterval( this.render, 1000/24);
+    update() {
+        GirlFriend.updateFrame()
+        Boyfriend.updateFrame()
+    },
+
+    pause() {
+        clearInterval(this.updateInterval)
+        clearInterval(this.renderInterval)
+        this.isPaused = true
+        this.pauseScreen = new Pause({ renderCb:  Tutorial.render  })
+        console.log("pause");
+    },
+
+    init() {
+        this.renderInterval = setInterval(() => this.render(), 1000 / 30)
+        this.updateInterval = setInterval(() => this.update(), 1000 / 24)
+        window.onkeydown = this.onkeydown
+    },
+
+    onkeydown(event) {
+        const { key } = event
+        buttonsFunctions?.[key]?.()
     }
 
+}
+
+const functions = {
+    down: () => { },
+    up: () => { },
+    pause: () => Tutorial.pause()
+}
+
+const buttonsFunctions = {
+    "s": functions.down,
+    "ArrowDown": functions.down,
+    "w": functions.up,
+    "ArrowUp": functions.up,
+    "Escape": functions.pause
 }
 
 
