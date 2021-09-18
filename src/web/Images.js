ea2bcf2f5917698ad4b829e62884ae019cb26522
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-09-18 18:21:04 -0300

    add more images and sprites in the module

--- src/web/Images.js
+++ src/web/Images.js
@@ -5,17 +5,26 @@ import { path as logoBumpinPath } from "../../assets/animations/logoBumpin.js"
 import { path as gfDanceTitlePath } from "../../assets/animations/gfDanceTitle.js"
 import { path as titleEnterPath } from "../../assets/animations/titleEnter.js"
 import { path as mainMenuButtonsPath} from "../../assets/animations/main_menu_buttons.js"
+import { path as alphabetPath } from "../../assets/animations/alphabet.js"
+import { path as GFAssetsPath} from "../../assets/animations/GF-assets.js"
+import { path as BOYFRIENDPath} from "../../assets/animations/BOYFRIEND.js"
 
 const Images = {
     backgroudYellow: await loadImage(imagesFolder, "menuBG.png"),
     backgroudBlue: await loadImage(imagesFolder, 'menuBGBlue.png'),
+    stageback: await loadImage(imagesFolder, "stageback.png"),
+    curtains: await loadImage(imagesFolder, "stagecurtains.png"),
+    stagefront: await loadImage(imagesFolder, "stagefront.png")
 }
 
 const Sprites = {
     girlFriendTitle: await loadImage(spritesFolder, gfDanceTitlePath),
     logo: await loadImage(spritesFolder, logoBumpinPath),
     titleEnter: await loadImage(spritesFolder, titleEnterPath),
-    mainMenuButtons: await loadImage(spritesFolder, mainMenuButtonsPath)
+    mainMenuButtons: await loadImage(spritesFolder, mainMenuButtonsPath),
+    alphabet: await loadImage(spritesFolder, alphabetPath),
+    GFAssets: await loadImage(spritesFolder, GFAssetsPath),
+    BOYFRIEND: await loadImage(spritesFolder, BOYFRIENDPath),
 }
 
 export { Sprites, Images }
