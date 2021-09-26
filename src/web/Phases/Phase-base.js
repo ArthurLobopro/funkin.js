4ddb7869f551704a6b0f23939e33f70adfb96480
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-09-26 16:39:36 -0300

    add Boyfriend and Girlfriend

--- src/web/Phases/Phase-base.js
+++ src/web/Phases/Phase-base.js
@@ -1,19 +1,30 @@
 import { Pause } from "../Screens/Pause.js";
 import { ArrowsPainel } from "../Components/Arrows.js";
+import { Boyfriend } from "../Components/Boyfriend.js";
+import { Girlfriend } from "../Components/Girlfriend.js";
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
 export class Phase {
-    pauseScreen
-    ArrowsPainel
-
     constructor({ renderBackground }){
         this.ArrowsPainel = new ArrowsPainel()
+        this.GirlFriend = new Girlfriend()
+        this.Boyfriend = new Boyfriend()
         this.renderBackground = renderBackground
+        this.pauseScreen = null
     }
 
     render(){
         this.renderBackground?.()
+        this.GirlFriend.render()
+        this.Boyfriend.render()
+        this.ArrowsPainel.render()
+    }
+
+    update(){
+        this.ArrowsPainel.update()
+        this.GirlFriend.update()
+        this.Boyfriend.update(this.GirlFriend)
     }
 
     pause(){
