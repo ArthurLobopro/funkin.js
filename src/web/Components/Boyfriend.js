7184e8898c991da87bcd0425a2e6e65133befe1f
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-09-26 16:23:53 -0300

    Create Boyfriend.js

---
+++ src/web/Components/Boyfriend.js
@@ -0,0 +1,36 @@
+import { types } from "../../../assets/animations/BOYFRIEND.js"
+import { multframesAnimations } from "../Animation.js"
+import { Sprites } from "../Images.js"
+
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+
+export class Boyfriend extends multframesAnimations {
+    constructor({ x, y }) {
+        super({ types, frames: types.frames["idle-dance"] })
+        this.x = x
+        this.y = y
+    }
+
+    update(GirlFriend) {
+        this.x = GirlFriend.x + GirlFriend.width / 2
+        this.y = GirlFriend.y + GirlFriend.height / 2
+        this.updateFrame()
+    }
+
+    render() {
+        const { x: sx, y: sy, width, height, frameHeight = 0, frameWidth = 0, frameX = 0, frameY = 0 } = this.atualFrame
+        const dh = (frameHeight || height) / 1.5
+        const dw = (frameWidth || width) / 1.5
+        const x = this.x
+        const y = this.y
+
+        ctx.drawImage(
+            Sprites.BOYFRIEND,
+            sx, sy,
+            width, height,
+            x, y,
+            dw, dh
+        )
+    }
+}
