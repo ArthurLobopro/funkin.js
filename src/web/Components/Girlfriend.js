f7afc1d8e035735c9f69a1e3921bc69ca31f6b26
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-09-26 16:23:57 -0300

    Create Girlfriend.js

---
+++ src/web/Components/Girlfriend.js
@@ -0,0 +1,43 @@
+import { frames as frameTypes } from "../../../assets/animations/GF-assets.js"
+import { multframesAnimations } from "../Animation.js"
+import { Sprites } from "../Images.js"
+
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+
+export class Girlfriend extends multframesAnimations {
+    constructor() {
+        super({ frames: frameTypes.dancingBeat, types: frameTypes })
+        this.updateCoords()
+    }
+
+    updateCoords() {
+        const { width, height, frameHeight = 0, frameWidth = 0 } = this.atualFrame
+
+        this.width = (frameWidth || width) / 1.5
+        this.height = (frameHeight || height) / 1.5
+        this.x = canvas.width / 2 - this.width / 2
+        this.y = canvas.height / 2 - this.height / 2
+    }
+
+    update() {
+        this.updateFrame()
+        this.updateCoords()
+    }
+
+    render() {
+        const { x: sx, y: sy, width, height } = this.atualFrame
+        const dh = this.height
+        const dw = this.width
+        const x = this.x
+        const y = this.y
+
+        ctx.drawImage(
+            Sprites.GFAssets,
+            sx, sy,
+            width, height,
+            x, y,
+            dw, dh
+        )
+    }
+}
