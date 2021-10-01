7b3e015fd880a458e962d821c9faee13355bd6c8
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-09-30 22:22:07 -0300

    move Animation

--- src/web/Components/Animation.js
+++ src/web/Components/Animation.js
@@ -1,4 +1,6 @@
-import { delay } from "./Util.js"
+//@ts-check
+
+import { delay } from "../Util.js"
 
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
@@ -24,49 +26,6 @@ class animationBase {
     updateFrame() {
         this.atualFrameIndex = this.atualFrameIndex + 1 === this.frames.length ? 0 : this.atualFrameIndex + 1
         this.atualFrame = this.frames[this.atualFrameIndex]
-    }
-}
-
-class menuButtonsAnimationBase {
-    isFocused = false
-    constructor({ types, ...atributes }) {
-        Object.entries(atributes).forEach(([key, value]) => {
-            this[key] = value
-        })
-
-        console.log(atributes);
-        this.types = types
-        this.frames = this.types[this.isFocused ? 'focus' : 'basic']
-        this.atualFrame = this.frames[0]
-        this.atualFrameIndex = 0
-
-    }
-    updateFrame() {
-        this.atualFrameIndex = this.atualFrameIndex + 1 === this.frames.length ? 0 : this.atualFrameIndex + 1
-        this.atualFrame = this.frames[this.atualFrameIndex]
-    }
-    reset() {
-        this.atualFrameIndex = 0
-        this.frames = this.types[this.isFocused ? 'focus' : 'basic']
-        this.atualFrame = this.frames[0]
-    }
-    focus() {
-        if (this.isFocused)
-            return
-
-        this.isFocused = true
-        this.frames = this.types.focus
-        this.atualFrameIndex = 0
-        this.atualFrame = this.frames[0]
-    }
-    unFocus() {
-        if (!this.isFocused)
-            return
-
-        this.isFocused = false
-        this.frames = this.types.basic
-        this.atualFrameIndex = 0
-        this.atualFrame = this.frames[0]
     }
 }
 
@@ -120,4 +79,4 @@ async function transition(renderCallBack, middleCallBack) {
     }
 }
 
-export { animationBase, menuButtonsAnimationBase, multframesAnimations, transition }
+export { animationBase, multframesAnimations, transition }
