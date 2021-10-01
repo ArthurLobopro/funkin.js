06cf7269aa091250e83775866e830827b4394931
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-09-30 22:21:45 -0300

    implement MenuButtonsList

--- src/web/Screens/Menu.js
+++ src/web/Screens/Menu.js
@@ -1,11 +1,10 @@
-const { shell } = require('electron')
 import { range, delay, playAudio } from '../Util.js'
-import { menuButtonsAnimationBase, transition } from '../Animation.js'
-import * as mainButtons from "../../../assets/animations/main_menu_buttons.js"
+import { transition } from '../Components/Animation.js'
 import { Sounds } from "../Audio.js"
 import { Start } from './Start.js'
 import { FreePlay } from './FreePlay.js'
 import { Images, Sprites } from '../Images.js'
+import { MenuButtonsList } from '../Components/MenuButton.js'
 
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
@@ -15,104 +14,17 @@ const images = {
     buttons: Sprites.mainMenuButtons
 }
 
-const screenComponents = {
-    buttons: {
-        storyMode: new menuButtonsAnimationBase({
-            types: mainButtons.storymode,
-            isFocused: true,
-            draw() {
-                const { x: sx, y: sy, width, height, frameX = 0, frameY = 0, frameWidth = 0, frameHeight = 0 } = this.atualFrame
-                const dh = frameHeight || height
-                const dw = frameWidth || width
-                const x = (canvas.width / 2 - dw / 2) - frameX
-                const y = 50 + frameY
-                ctx.drawImage(
-                    images.buttons,
-                    sx, sy,
-                    width, height,
-                    x, y,
-                    dw, dh
-                )
-            },
-            click() {
-
-            }
-        }),
-        freeplay: new menuButtonsAnimationBase({
-            types: mainButtons.freeplay,
-            draw() {
-                const { x: sx, y: sy, width, height, frameX = 0, frameY = 0, frameWidth = 0, frameHeight = 0 } = this.atualFrame
-                const dh = frameHeight || height
-                const dw = frameWidth || width
-                const x = (canvas.width / 2 - dw / 2) - frameX
-                const y = 100 + height + frameY - (this.isFocused ? 50 : 0)
-                ctx.drawImage(
-                    images.buttons,
-                    sx, sy,
-                    width, height,
-                    x, y,
-                    dw, dh
-                )
-            },
-            click() {
-                Menu.reset()
-                FreePlay.init()
-            }
-        }),
-
-        donate: new menuButtonsAnimationBase({
-            types: mainButtons.donate,
-            draw() {
-                const { x: sx, y: sy, width, height, frameX = 0, frameY = 0, frameWidth = 0, frameHeight = 0 } = this.atualFrame
-                const dh = frameHeight || height
-                const dw = frameWidth || width
-                const x = (canvas.width / 2 - width / 2) + frameX / 2
-                const y = 150 + dh * 2 - frameY - (this.isFocused ? 70 : 0)
-                ctx.drawImage(
-                    images.buttons,
-                    sx, sy,
-                    width, height,
-                    x, y,
-                    dw, dh
-                )
-            },
-            click() {
-                shell.openExternal("https://ninja-muffin24.itch.io/funkin")
-            }
-        }),
-    }
-}
-
 const functions = {
     down() {
-        const buttons = Object.entries(screenComponents.buttons)
-        const focusedIndex = buttons.findIndex(([key, component]) => component.isFocused)
-        buttons.forEach(([key, component]) => {
-            component.unFocus()
-        })
-
-        const newIndex = focusedIndex + 1 === buttons.length ? 0 : focusedIndex + 1
-
-        buttons[newIndex][1].focus()
-        moveBackground(newIndex)
-        playAudio(Sounds.scrollMenu)
+        Menu.buttons.down()
+        moveBackground(Menu.buttons.focusedIndex)
     },
     up() {
-        const buttons = Object.entries(screenComponents.buttons)
-        const focusedIndex = buttons.findIndex(([key, component]) => component.isFocused)
-        buttons.forEach(([key, component]) => {
-            component.unFocus()
-        })
-
-        const newIndex = focusedIndex === 0 ? buttons.length - 1 : focusedIndex - 1
-        buttons[newIndex][1].focus()
-        moveBackground(newIndex)
-        playAudio(Sounds.scrollMenu)
+        Menu.buttons.up()
+        moveBackground(Menu.buttons.focusedIndex)
     },
     click() {
-        const buttons = Object.entries(screenComponents.buttons)
-        const focusedButton = buttons.find(([key, component]) => component.isFocused)
-        focusedButton[1].click()
+        Menu.buttons.click()
     },
     returnToStart() {
         clearInterval(Menu.renderInterval)
@@ -148,6 +60,7 @@ const Menu = {
     renderInterval: null,
     updateInterval: null,
     backgroudY: 0,
+    buttons: new MenuButtonsList(),
     render() {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         const { backgroudYellow: backgroud } = images
@@ -158,18 +71,14 @@ const Menu = {
             0, 0,
             canvas.width, canvas.height
         )
-        Object.entries(screenComponents.buttons).forEach(([key, component]) => {
-            component.draw()
-        })
+        this.buttons.render()
     },
-    updateFrames() {
-        Object.entries(screenComponents.buttons).forEach(([key, component]) => {
-            component.updateFrame()
-        })
+    update() {
+        this.buttons.update()
     },
     init() {
-        this.renderInterval = setInterval(Menu.render, 1000 / 60);
-        this.updateInterval = setInterval(Menu.updateFrames, 1000 / 18)
+        this.renderInterval = setInterval(() => Menu.render() , 1000 / 60);
+        this.updateInterval = setInterval(() => Menu.update(), 1000 / 18)
         window.onkeydown = this.onkeydown
     },
     reset() {
@@ -183,7 +92,6 @@ const Menu = {
         const { key } = event
         buttonsFunctions?.[key]?.()
     }
-
 }
 
 export { Menu }
